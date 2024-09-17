796627579ce968f501e2aa6f4cc601c0df76cfd6
DiegoGuayara <diegofguayara9707@gmail.com>
2024-09-17 12:15:00 -0500

    hola

---
+++ dom/js/ejerciciosBlog.js
@@ -0,0 +1,6 @@
+function cambiarColor(color) {
+  const t = document.getElementById("titulo");
+  const r = document.getElementById("titulo2");
+  t.style.color = color;
+  r.style.color = color;
+}
