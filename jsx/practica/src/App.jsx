9b0cdbc2c846f1492ceb7225df006f1436f9bcb2
DiegoGuayara <diegofguayara9707@gmail.com>
2024-09-23 22:55:11 -0500

    hola

--- jsx/practica/src/App.jsx
+++ jsx/practica/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
 let prg = parseInt(prompt("Ingrese un numero"));
+
 function App() {
   if (prg % 2 === 0) {
-    return <h1>Es un numero par</h1>;
+    return <h1>Es par</h1>;
+  } else if (isNaN(prg)) {
+    return <h1>Ingrese un numero</h1>;
   } else {
-    return <h1>Es un numero impar</h1>;
+    return <h1>Es impar</h1>;
   }
 }
 
