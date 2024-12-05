41a1bbd045d226648e163c718f6d413a70df663d
DiegoGuayara <diegofguayara9707@gmail.com>
2024-12-05 16:14:14 -0500

    ultimos cambios

--- html/dom/js/mokepon.js
+++ html/dom/js/mokepon.js
@@ -1,16 +1,90 @@
+//Elementos html
 let botonMascotaJugador = document.getElementById("boton-mascota");
 let inputHipodoge = document.getElementById("hipodoge");
 let inputCapipepo = document.getElementById("capipepo");
 let inputRatigueya = document.getElementById("ratigueya");
+let spanMascotaJugador = document.getElementById("mascota-jugador");
+let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+let botonFuego = document.getElementById("boton-fuego");
+let botonAgua = document.getElementById("boton-agua");
+let botonTierra = document.getElementById("boton-tierra");
+let sectionMensajes = document.getElementById("mensajes");
 
+//Variables
+let ataqueJugador;
+let ataqueEnemigo;
+
+//Eventos
 botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+botonFuego.addEventListener("click", ataqueFuego);
+botonAgua.addEventListener("click", ataqueAgua);
+botonTierra.addEventListener("click", ataqueTierra);
 
 function seleccionarMascotaJugador() {
   if (inputHipodoge.checked) {
-    alert("Seleccionaste a Hipodoge");
+    spanMascotaJugador.innerHTML = "Hipodoge";
   } else if (inputCapipepo.checked) {
-    alert("Seleccionaste a Capipepo");
+    spanMascotaJugador.innerHTML = "Capipepo";
   } else if (inputRatigueya.checked) {
-    alert("Seleccionaste a Ratigueya");
+    spanMascotaJugador.innerHTML = "Ratigueya";
+  } else {
+    alert("Selecciona a tu mascota");
   }
+
+  seleccionarMascotaEnemigo();
+}
+
+function seleccionarMascotaEnemigo() {
+  let mascotaAleatoria = aleatorio(1, 3);
+
+  if (mascotaAleatoria === 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodoge";
+  } else if (mascotaAleatoria === 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo";
+  } else {
+    spanMascotaEnemigo.innerHTML = "Ratigueya";
+  }
+}
+
+function ataqueFuego() {
+  ataqueJugador = "FUEGO";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueAgua() {
+  ataqueJugador = "AGUA";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueTierra() {
+  ataqueJugador = "TIERRA";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueAleatorioEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 3);
+
+  if (ataqueAleatorio === 1) {
+    ataqueEnemigo = "FUEGO";
+  } else if (ataqueAleatorio === 2) {
+    ataqueEnemigo = "AGUA";
+  } else {
+    ataqueEnemigo = "TIERRA";
+  }
+  crearMensaje();
+}
+
+function combate(){
+  
+}
+
+function crearMensaje() {
+  let parrafo = document.createElement("p");
+  parrafo.innerHTML = `Tu mascota atacó con ${ataqueJugador}, la mascota del enemigo atacó con ${ataqueEnemigo} - PENDIENTE`;
+
+  sectionMensajes.appendChild(parrafo);
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
