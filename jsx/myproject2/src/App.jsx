7f7c0f1594a04eff1220a51146f5b8b3a24a7ab8
DiegoGuayara <diegofguayara9707@gmail.com>
2025-02-27 15:36:27 -0500

    ultimos cambios

--- jsx/myproject2/src/App.jsx
+++ jsx/myproject2/src/App.jsx
@@ -2,23 +2,50 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [texto, setTexto] = useState('')
+  const [textoUser, setTextoUser] = useState("");
+  const [mensaje, setMensaje] = useState("");
 
-  function datosUser(){    
+  const [password, setPassword] = useState("");
+  const [mensaje2, setMensaje2] = useState("");
+
+  function nombreUser() {
+    if (textoUser === "") setMensaje("Ingrese su nombre de usuario por favor");
+    else setMensaje(`Bienvenido ${textoUser}`);
+  }
+
+  function clavePassword() {
+    if (password === "") setMensaje2("Ingrese la contraseña por favor");
+    else setMensaje2("Contraseña ingresada");
   }
 
   return (
     <div>
       <main>
         <h1>Login</h1>
-        <input 
-        type="text" 
-        placeholder="Username"
-        onChange={(e) => setTexto(e.target.value)}/>
-        <input type="password" placeholder="Password"/>
-        <a href="">Forgot your Password?</a>
-        <button>Login</button>
-        
+        <input
+          type="text"
+          placeholder="Username"
+          value={textoUser}
+          onChange={(e) => setTextoUser(e.target.value)}
+        />
+        {mensaje && <p id="mens1">{mensaje}</p>}
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {mensaje2 && <p id="mens2">{mensaje2}</p>}
+        <a href="#">Forgot your Password?</a>
+        <button
+          onClick={() => {
+            nombreUser();
+            clavePassword();
+          }}
+        >
+          Login
+        </button>
       </main>
     </div>
   );
