3e09293ae43ebe1912ed68ab97092946b2c60159
DiegoGuayara <diegofguayara9707@gmail.com>
2025-02-26 20:50:59 -0500

    Ultimos cambios

--- jsx/myproject/src/App.jsx
+++ jsx/myproject/src/App.jsx
@@ -1,3 +1,13 @@
+import Practica1 from "./Practica1.jsx";
+import Practica2 from "./Practica2.jsx";
+import Practica3 from "./Practica3.jsx";
+import Practica4 from "./Practica4.jsx";
+import Practica5 from "./Practica5.jsx";
+import Practica6 from "./Practica6.jsx";
+import Practica7 from "./Practica7.jsx";
+import Practica8 from "./Practica8.jsx";
+import Practica9 from "./Practica9.jsx";
+import Practica10 from "./Practica10.jsx";
 import { useState } from "react";
 
 function evento() {
@@ -20,6 +30,17 @@ function App() {
       <button style={{ backgroundColor: color }} onClick={cambiarColor}>
         Haz click para cambiar el color
       </button>
+    <Practica1/>
+    <Practica2/>
+    <Practica3/>
+    <Practica4/>
+    <Practica5/>
+    <Practica6/>
+    <Practica7/>
+    <Practica8/>
+    <Practica9/>
+    <Practica10/>
+
     </div>
   );
 }
--- jsx/myproject/src/main.jsx
+++ jsx/myproject/src/main.jsx
@@ -1,30 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Practica1 from "./Practica1.jsx";
-import Practica2 from "./Practica2.jsx";
-import Practica3 from "./Practica3.jsx";
-import Practica4 from "./Practica4.jsx";
-import Practica5 from "./Practica5.jsx";
-import Practica6 from "./Practica6.jsx";
-import Practica7 from "./Practica7.jsx";
-import Practica8 from "./Practica8.jsx";
-import Practica9 from "./Practica9.jsx";
-import Practica10 from "./Practica10.jsx";
 
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <App />
-    <Practica1 />
-    <Practica2 />
-    <Practica3 />
-    <Practica4 />
-    <Practica5 />
-    <Practica6 />
-    <Practica7 />
-    <Practica8 />
-    <Practica9 />
-    <Practica10 />
-  </StrictMode>
 );
