e66bc814cdcc1e89f5ff2cc94728463f1436d5d5
DiegoGuayara <diegofguayara9707@gmail.com>
2025-03-19 19:16:06 -0500

    Prueba

--- java/Swing.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Swing {
-    public static void main(String args[]){
-        String nombre = JOptionPane.showInputDialog("Cual es tu nombre");
-        System.out.printf("Hola %s", nombre);
-    }
-}
