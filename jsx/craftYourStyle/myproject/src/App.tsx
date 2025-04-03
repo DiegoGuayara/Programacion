598fb56c34e8f7c6bf349ee83a9ec6b2fef4e733
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-02 20:42:30 -0500

    dwedwedwedwe

---
+++ jsx/craftYourStyle/myproject/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Header } from "./Header/Header";
+import { L_items } from "./L-items/L-items";
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <L_items />
+    </div>
+  );
+};
---
+++ jsx/craftYourStyle/myproject/src/L-items/L-items.tsx
@@ -0,0 +1,15 @@
+import "./L-items.css";
+
+export const L_items = () => {
+  return (
+    <div>
+      <div className="cont1">
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+};
